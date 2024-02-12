a70550d58a799ae58bf96a3c93e11ab93fb36f48
Nicolas Peralta <nperaltapz@gmail.com>
2024-02-11 23:24:41 -0500

    Designing app

---
+++ Presentation.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+st.image('images/black-logo.png')
+
+st.markdown("### What is this webpage?​​")
+st.markdown("""
+Welcome to Bloomstar, where data meets beauty! As a dedicated data professional, I am thrilled to present to you a personalized portfolio application crafted with speed and precision. While the data showcased here is purely illustrative, the potential for deeper analysis is boundless. Despite time constraints, this application showcases my passion for transforming data into actionable insights. Although there's room for improvement in terms of code modularization, commenting, and refactoring, these enhancements are reserved for future iterations when time permits. I am eager to discuss how this application can contribute to Bloomstar's success, leveraging the power of data to blossom into new opportunities. Let's embark on this journey together.
+""")
+
+st.image('images/Gerbera.jpg')
+
+st.markdown("### About Me:")
+st.markdown("""
+I am Nicolas Peralta, an experienced Data Professional with a relentless passion for data and a proven track record of delivering impactful solutions. My comprehensive skill set spans Data Science, Data Engineering, Data Analysis, Web Scraping, and Web Development. Throughout my career, I have consistently demonstrated my ability to leverage data-driven insights to drive strategic decision-making and fuel business growth.
+""")
+
+st.markdown(
+"""
+#### Key Skills:
+
+- **Data Science:** Proficient in leveraging statistical analysis, machine learning, and predictive modeling techniques to extract actionable insights from complex datasets, driving informed decision-making and uncovering hidden opportunities.
+
+- **Data Engineering:** Skilled in designing and implementing scalable data architectures, building robust data pipelines, and optimizing storage and processing systems for enhanced efficiency and performance.
+
+- **Data Analysis:** Experienced in performing exploratory data analysis, developing data visualizations, and generating insightful reports to communicate findings and drive actionable outcomes.
+
+- **Web Scraping:** Proficient in utilizing web scraping techniques and tools to gather and extract data from various online sources, enabling comprehensive data collection and analysis.
+
+- **Web Development:** Competent in developing web applications and implementing interactive data visualizations using modern web technologies, enhancing user engagement and facilitating data-driven decision-making.
+
+
+"""
+)
+
+linkedin.com/in/nicolas-peralta-paez
